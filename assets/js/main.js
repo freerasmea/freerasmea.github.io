933f9b978bcdfe14e7a15df92d40d50a799f1ad4
Free Rasmea <freerasmea@riseup.net>
2015-03-10 22:02:01 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,8 +39,7 @@ $(document).ready(function() {
     function setRandomPhrase() {
         // Set phrases into an array
         var phrases = new Array(
-            "A Message from Rasmea <a href="{{site.baseurl}}news/2015/03/10/a-speech-from-rasmea-on-International-Women's-Day/" class="button">Read More</a>
-            
+            "A Message from Rasmea <a href="{{site.baseurl}}news/2015/03/10/a-speech-from-rasmea-on-International-Women's-Day/" class="button">Read More</a>"
         );
 
         // Selects a random phrase
